2fa06ec43c54adf8a3cec039cc5609c0c442b907
JoseB <jmanuel.benitez@udea.edu.co>
2017-09-10 13:26:59 -0500

    Erase database function

--- app/src/main/java/com/example/jose/sensemonitor/BlueConnectionService.java
+++ app/src/main/java/com/example/jose/sensemonitor/BlueConnectionService.java
@@ -396,14 +396,8 @@ public class BlueConnectionService {
             // Get a BluetoothSocket for a connection with the
             // given BluetoothDevice
             try {
-                if (secure) {
-                    tmp = device.createRfcommSocketToServiceRecord(
+                tmp = device.createRfcommSocketToServiceRecord(
                             MY_UUID_SECURE);
-                } else {
-                    Log.d("[BICHO]", "creating RF commsocket");
-                    tmp = device.createInsecureRfcommSocketToServiceRecord(
-                            MY_UUID_INSECURE);
-                }
             } catch (IOException e) {
                 Log.e(TAG, "Socket Type: " + mSocketType + "create() failed", e);
             }
